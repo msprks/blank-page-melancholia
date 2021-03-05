c7d0cf2c998bfa49c64cbc3e0473f64f4ee8c2e6
M.E. Sparks <msparks@MEs-MacBook-Air.local>
2021-03-05 14:51:54 -0800

    idea

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ $(".output-wrapper").bind("click", ()=>{
 
 poemList=[
 function poem1(){
-	let poem = "Within this "+random(ADJECTIVES)+" headspace, caught between memories of your "+random(NOUNS)+" and my "+random(NOUNS)+", an idea forms: '"+random(ADJECTIVES)+" "+random(NOUNS)+"', I think.";
+	let poem = "Within this "+random(ADJECTIVES)+" headspace, caught between your "+random(NOUNS)+" and my "+random(NOUNS)+", an idea begins to form: '"+random(ADJECTIVES)+" "+random(NOUNS)+"', I think.";
 	return poem
 },
 function poem2(){
@@ -108,7 +108,7 @@ function poem5(){
 	return poem
 },
 function poem6(){
-	let poem = "Hold on to the "+random(NOUNS)+". I should keep going, keep "+random(VERBSVBG)+".";
+	let poem = "I'll hold on to the "+random(NOUNS)+". Keep going, keep "+random(VERBSVBG)+".";
 	return poem
 },
 function poem7(){
