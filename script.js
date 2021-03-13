d3f529027e3ebf4d36f9a801ac375db824bbece7
M.E. Sparks <msparks@MEs-MacBook-Air.local>
2021-03-13 14:12:59 -0800

    keeps

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function poem5(){
 	return poem
 },
 function poem6(){
-	let poem = "Don't let go of the "+random(NOUNS)+" If you keep hold, I'll keep "+random(VERBSVBG)+".";
+	let poem = "Hold on to the "+random(NOUNS)+". I'll keep "+random(VERBSVBG)+".";
 	return poem
 },
 function poem7(){
