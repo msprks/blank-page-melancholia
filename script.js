adbbbe4f4c3b307b52b06c2d5e452081da6a822c
M.E. Sparks <msparks@MEs-MacBook-Air.local>
2021-03-13 14:00:16 -0800

    title

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function poem5(){
 	return poem
 },
 function poem6(){
-	let poem = "Hold on to the "+random(NOUNS)+". I'll keep going, keep "+random(VERBSVBG)+".";
+	let poem = "Hold on to the "+random(NOUNS)+". Remember to "+random(VERBSVB)+". I'll keep going, keep "+random(VERBSVBG)+".";
 	return poem
 },
 function poem7(){
